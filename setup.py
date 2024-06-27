cc6d57404d6c91c0379e9d0adb7ae781480fc56a
Anatoly <acryptokrestenko@gmail.com>
2024-06-27 22:30:08 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='fractal',
+    name='fractal-defi',
     version='1.0.0',
     packages=find_packages(),
     author='Logarithm Labs',
@@ -10,9 +10,10 @@ setup(
     description='An ultimate DeFi research library for strategy development and fractaling.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Logarithm-Labs/fractal',
+    url='https://github.com/Logarithm-Labs/Fractal',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: BSD-3-Clause',
-    ]
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
 )
