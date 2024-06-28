fa3a277b6c0b41204244bd16b69bf38bb2337794
Anatoly <acryptokrestenko@gmail.com>
2024-06-28 18:43:28 +0400

    v1.0.1 - Optimize dependencies & building

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
 
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
+
 setup(
     name='fractal-defi',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     author='Logarithm Labs',
     author_email='dev@logarithm.fi',
@@ -11,6 +16,9 @@ setup(
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Logarithm-Labs/Fractal',
+    include_package_data=True,
+    python_requires=">=3.8, <4",
+    install_requires=parse_requirements('requirements.txt'),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: BSD License',
