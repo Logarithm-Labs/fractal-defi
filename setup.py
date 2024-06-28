b55aa32fdcca37ef99760770d0d792e0737d1933
Anatoly <acryptokrestenko@gmail.com>
2024-06-28 20:00:15 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 
 setup(
     name='fractal-defi',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     author='Logarithm Labs',
     author_email='dev@logarithm.fi',
