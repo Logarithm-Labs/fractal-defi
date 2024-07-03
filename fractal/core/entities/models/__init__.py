31c14f8af4c9be607ed6eeb1fc5353c4610441c7
Anatoly <acryptokrestenko@gmail.com>
2024-07-03 17:12:08 +0400

    Squashed commit of the following:
    commit 9929b629ec8b40b8d737b5b3f14398d7f45d2a00
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Fri Jun 28 20:00:15 2024 +0400
    
        Update setup.py
    
    commit 61ffe3bd796f87f66262267797301601bd1b9fca
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Fri Jun 28 19:35:56 2024 +0400
    
        v1.0.1 - Fix models/uniswap_v3_fees import path
    
    commit 26529d7678e0c8391b30774d7e9722dfbf4d05ef
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Fri Jun 28 18:43:28 2024 +0400
    
        v1.0.1 - Optimize dependencies & building
    
    commit 05fa0300d2b7a757c25993316e938fd79a09b346
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Thu Jun 27 22:39:28 2024 +0400
    
        Update Makefile
    
    commit b25c0d7bacd9653d407f570593bc0b59521119ec
    Merge: cc6d574 fcd6752
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Thu Jun 27 22:30:18 2024 +0400
    
        Merge branch 'main' of https://github.com/Logarithm-Labs/Fractal
    
    commit cc6d57404d6c91c0379e9d0adb7ae781480fc56a
    Author: Anatoly <acryptokrestenko@gmail.com>
    Date:   Thu Jun 27 22:30:08 2024 +0400
    
        Update setup.py
    
    commit fcd675204c3097c453dc32698683ec0f705fb204
    Merge: 265d082 31ecc0d
    Author: likeblood <48191103+likeblood@users.noreply.github.com>
    Date:   Thu Jun 27 22:05:41 2024 +0400
    
        Merge pull request #1 from Logarithm-Labs/dev
    
        v1.0.0 - Add rst documentation

---
+++ fractal/core/entities/models/__init__.py
@@ -0,0 +1,6 @@
+from fractal.core.entities.models.uniswap_v3_fees import estimate_fee, get_liquidity_delta
+
+__all__ = [
+    "estimate_fee",
+    "get_liquidity_delta",
+]
--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 from setuptools import setup, find_packages
 
 
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return [line.strip() for line in f if line and not line.startswith('#')]
+
+
 setup(
-    name='fractal',
-    version='1.0.0',
+    name='fractal-defi',
+    version='1.0.2',
     packages=find_packages(),
     author='Logarithm Labs',
     author_email='dev@logarithm.fi',
     description='An ultimate DeFi research library for strategy development and fractaling.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/Logarithm-Labs/fractal',
+    url='https://github.com/Logarithm-Labs/Fractal',
+    include_package_data=True,
+    python_requires=">=3.8, <4",
+    install_requires=parse_requirements('requirements.txt'),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: BSD-3-Clause',
-    ]
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
 )
